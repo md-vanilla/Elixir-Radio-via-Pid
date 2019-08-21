1f0ab95a9728ab1072f751e77380898caefece13
michaeldimmitt <michaelgdimmitt@gmail.com>
2019-08-21 13:29:05 -0400

    add stream func for station and loop for radio to connect or disconnect

--- lib/alternate_solution/elixir_radio.ex
+++ lib/alternate_solution/elixir_radio.ex
@@ -12,7 +12,20 @@ defmodule ElixirCarRadio do
   # car radio can be off.
   # car radio turns on and goes connects back to previous connected station.
   # car radio lost power and defaults to FM600.
+  def loop() do
+    receive do
+      {:connect, msg}  -> "will match" <> " " <> msg |> IO.inspect;loop()
+      {:connect, msg} ->  "wont match" <> " " <> msg |> IO.inspect;loop()
+      {:disconnect} -> "disconnected"
+      {:disconnect, _msg} -> "disconnected"
+      {_, _msg} -> "invalid pattern sent to loop in Elixir Radio" |> IO.inspect; loop()
+    end
+  end
 end
+send self(), {:connect, "connect reached"} |> IO.inspect()
+send self(), {:disconnect, "disconnect reached"} |> IO.inspect()
+
+ElixirRadio.loop()
 
 defmodule Station do
   # 1) each station will only know about itself.
@@ -20,4 +33,8 @@ defmodule Station do
   # 3) station registers itself to pubsub registry
   # 4) station brodcasts itself and others the top of its infinate cycle.
   # 5) when a radio sends "on" the station add that radio to a pubsub registry.
+
+  def stream do
+    Stream.cycle(1..10)
+  end
 end
